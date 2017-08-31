d8bf30ded32a060326967c68737d9cddf46a65d7
Maximilian Schieder <maximilian.schieder@posteo.net>
2017-08-31 17:06:15 +0200

    Add schema for Users Table

---
+++ database/migrations/20170831150447_create_users_table.php
@@ -0,0 +1,26 @@
+<?php
+  use Zeus\Database\Migrations\Migration;
+  use Illuminate\Database\Schema\Blueprint;
+
+  class CreateUsersTable extends Migration
+  {
+    public function up()
+    {
+      $this->schema->create('users', function(Blueprint $table) {
+        $table->increments('id');
+        $table->string('username')->unique;
+        $table->string('email')->nullable()->unique;
+        $table->string('first_name')->nullable();
+        $table->string('last_name')->nullable();
+        $table->string('company')->nullable();
+        $table->string('vat_id')->nullable();
+        $table->string('password');
+        $table->timestamps();
+      });
+    }
+
+    public function down()
+    {
+      $this->schema->drop('users');
+    }
+  }
