280f6e2f1c5dc23f539d01613cfd9c73abdd6086
Maximilian Schieder <maximilian.schieder@posteo.net>
2017-08-31 16:51:24 +0200

    Add Phinx for database migrations

---
+++ app/Database/Migrations/MigrationStub.php
@@ -0,0 +1,16 @@
+<?php
+  use Illuminate\Database\Schema\Blueprint;
+  use $useClassName;
+
+  class $className extends $baseClassName
+  {
+    public function up()
+    {
+      //
+    }
+
+    public function down()
+    {
+      //
+    }
+  }
