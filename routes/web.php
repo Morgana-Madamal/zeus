9ed23820a553d9a092ce4df7544630aa69f3fce8
Maximilian Schieder <maximilian.schieder@posteo.net>
2017-09-01 15:13:11 +0200

    Create auth route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
 use Zeus\Controllers\HomeController;
+use Zeus\Controllers\Auth\AuthController;
 
-$app->get('/', HomeController::class . ':index');
+$app->get('/', HomeController::class . ':index')->setName('home');
+$app->get('/auth/signup', AuthController::class . ':getSignUp')->setName('auth.signup');
+$app->post('/auth/signup', AuthController::class . ':postSignUp');
